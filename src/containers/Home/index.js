8bc061330b6ae11c9ff964bddbabe6861c82a2e6
UnkAtreus <unk.atreus@gmail.com>
2021-11-08 20:07:56 +0700

    set age data

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,6 +3,8 @@ import { Layout, Menu, Card, Col, Row, Statistic } from "antd";
 import { Pie, Line, TinyColumn, TinyArea, TinyLine } from "@ant-design/charts";
 import {
   PieChartOutlined,
+  BarChartOutlined,
+  AppstoreOutlined
 } from "@ant-design/icons";
 
 const Home = () => {
@@ -24,6 +26,293 @@ const Home = () => {
         console.log("fetch data failed", error);
       });
   };
+
+  var dataAge = [
+    {
+      age: "20",
+      count: 10,
+    },
+    {
+      age: "21",
+      count: 8,
+    },
+    {
+      age: "22",
+      count: 5,
+    },
+    {
+      age: "23",
+      count: 2,
+    },
+    {
+      age: "24",
+      count: 2,
+    },
+    {
+      age: "25",
+      count: 4,
+    },
+    {
+      age: "26",
+      count: 4,
+    },
+    {
+      age: "27",
+      count: 8,
+    },
+    {
+      age: "28",
+      count: 2,
+    },
+    {
+      age: "29",
+      count: 1,
+    },
+    {
+      age: "30",
+      count: 6,
+    },
+    {
+      age: "31",
+      count: 4,
+    },
+    {
+      age: "32",
+      count: 10,
+    },
+    {
+      age: "33",
+      count: 7,
+    },
+    {
+      age: "34",
+      count: 16,
+    },
+    {
+      age: "35",
+      count: 22,
+    },
+    {
+      age: "36",
+      count: 14,
+    },
+    {
+      age: "37",
+      count: 13,
+    },
+    {
+      age: "38",
+      count: 18,
+    },
+    {
+      age: "39",
+      count: 19,
+    },
+    {
+      age: "40",
+      count: 11,
+    },
+    {
+      age: "41",
+      count: 14,
+    },
+    {
+      age: "42",
+      count: 16,
+    },
+    {
+      age: "43",
+      count: 12,
+    },
+    {
+      age: "44",
+      count: 12,
+    },
+    {
+      age: "45",
+      count: 9,
+    },
+    {
+      age: "46",
+      count: 17,
+    },
+    {
+      age: "47",
+      count: 12,
+    },
+    {
+      age: "48",
+      count: 15,
+    },
+    {
+      age: "49",
+      count: 16,
+    },
+    {
+      age: "50",
+      count: 6,
+    },
+    {
+      age: "51",
+      count: 9,
+    },
+    {
+      age: "52",
+      count: 16,
+    },
+    {
+      age: "53",
+      count: 13,
+    },
+    {
+      age: "54",
+      count: 17,
+    },
+    {
+      age: "55",
+      count: 16,
+    },
+    {
+      age: "56",
+      count: 11,
+    },
+    {
+      age: "57",
+      count: 15,
+    },
+    {
+      age: "58",
+      count: 13,
+    },
+    {
+      age: "59",
+      count: 23,
+    },
+    {
+      age: "60",
+      count: 19,
+    },
+    {
+      age: "61",
+      count: 12,
+    },
+    {
+      age: "62",
+      count: 14,
+    },
+    {
+      age: "63",
+      count: 17,
+    },
+    {
+      age: "64",
+      count: 13,
+    },
+    {
+      age: "65",
+      count: 17,
+    },
+    {
+      age: "66",
+      count: 23,
+    },
+    {
+      age: "67",
+      count: 9,
+    },
+    {
+      age: "68",
+      count: 8,
+    },
+    {
+      age: "69",
+      count: 4,
+    },
+    {
+      age: "70",
+      count: 3,
+    },
+    {
+      age: "71",
+      count: 1,
+    },
+    {
+      age: "72",
+      count: 1,
+    },
+    {
+      age: "73",
+      count: 2,
+    },
+    {
+      age: "74",
+      count: 0,
+    },
+    {
+      age: "75",
+      count: 5,
+    },
+    {
+      age: "76",
+      count: 3,
+    },
+    {
+      age: "77",
+      count: 0,
+    },
+    {
+      age: "78",
+      count: 1,
+    },
+    {
+      age: "79",
+      count: 2,
+    },
+    {
+      age: "80",
+      count: 0,
+    },
+    {
+      age: "81",
+      count: 0,
+    },
+    {
+      age: "82",
+      count: 0,
+    },
+    {
+      age: "83",
+      count: 0,
+    },
+    {
+      age: "84",
+      count: 1,
+    },
+    {
+      age: "85",
+      count: 0,
+    },
+    {
+      age: "86",
+      count: 1,
+    },
+    {
+      age: "87",
+      count: 0,
+    },
+    {
+      age: "88",
+      count: 0,
+    },
+    {
+      age: "89",
+      count: 0,
+    },
+    {
+      age: "90",
+      count: 1,
+    },
+  ];
 
   var dataPie = [
     {
@@ -72,8 +361,8 @@ const Home = () => {
 
   var configLine = {
     padding: "auto",
-    xField: "Date",
-    yField: "scales",
+    xField: "age",
+    yField: "count",
     xAxis: { tickCount: 5 },
     smooth: true,
   };
@@ -127,6 +416,14 @@ const Home = () => {
               <Menu.Item key="1" icon={<PieChartOutlined />}>
                 DashBoard
               </Menu.Item>
+              <Menu.SubMenu key="2" icon={<BarChartOutlined />} title="Category">
+                <Menu.Item key="21">Access to daylight</Menu.Item>
+                <Menu.Item key="22">Family zone in patient room</Menu.Item>
+                <Menu.Item key="23">Indoor lighting</Menu.Item>
+                <Menu.Item key="24">Noise reducing finishes</Menu.Item>
+                <Menu.Item key="25">Single bedroom</Menu.Item>
+                <Menu.Item key="26">Views of nature</Menu.Item>
+              </Menu.SubMenu>
             </Menu>
           </Layout.Sider>
           <Layout.Content className="p-5">
@@ -134,25 +431,25 @@ const Home = () => {
               <Row gutter={16}>
                 <Col span={6}>
                   <Card bordered={false}>
-                    <Statistic title="All Users" value={112893} />
+                    <Statistic title="All Users" value={42} />
                     <TinyColumn data={dataTinyColumn} {...configTinyColumn} />
                   </Card>
                 </Col>
                 <Col span={6}>
                   <Card bordered={false}>
-                    <Statistic title="Active Users" value={112893} />
+                    <Statistic title="Active Users" value={6} />
                     <TinyArea data={dataTinyColumn} {...configTinyAres} />
                   </Card>
                 </Col>
                 <Col span={6}>
                   <Card bordered={false}>
-                    <Statistic title="Form Submit" value={112893} />
+                    <Statistic title="Form Submit" value={360} />
                     <TinyColumn data={dataTinyColumn} {...configTinyColumn} />
                   </Card>
                 </Col>
                 <Col span={6}>
                   <Card bordered={false}>
-                    <Statistic title="Recenly Form" value={112893} />
+                    <Statistic title="Recenly Form" value={100} />
                     <TinyLine data={dataTinyColumn} {...configTinyLine} />
                   </Card>
                 </Col>
@@ -169,7 +466,7 @@ const Home = () => {
               </Row>
             </div>
             <div className="bg-white p-6 my-4">
-              <Line data={data} {...configLine} />
+              <Line data={dataAge} {...configLine} />
             </div>
           </Layout.Content>
         </Layout>
